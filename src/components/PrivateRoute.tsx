ec3c4b03f8ac211805e66f3ac0c269e4f1e9f1b1
Hồ Hữu Nhân <nhan111321@gmail.com>
2021-03-10 23:17:47 +0700

    commit

---
+++ src/components/PrivateRoute.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Route, RouteProps } from "react-router-dom";
+import { Login } from "../pages/Account/Login";
+
+export const PrivateRoute = ({
+  children,
+  ...rest
+}: RouteProps): JSX.Element => {
+  return (
+    <Route {...rest} render={() => (false ? children : <Login />)}></Route>
+  );
+};
