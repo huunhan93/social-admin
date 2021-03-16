af067f3d2273eab4363c9e8b0a787c5a939a1e37
Hồ Hữu Nhân <nhanhh.dtp@vnpt.vn>
2021-03-16 13:54:52 +0700

    add pagination user

---
+++ src/store/users/actions.ts
@@ -0,0 +1,30 @@
+import { Dispatch } from "react";
+import { userService } from "../../services";
+import {
+  LOAD_USERS_PAGING_FAILURE,
+  LOAD_USERS_PAGING_REQUEST,
+  LOAD_USERS_PAGING_SUCCESS,
+  UsersActionTypes,
+} from "./types";
+
+export const loadUserPaging = (currentPage: number) => {
+  return async (dispatch: Dispatch<UsersActionTypes>) => {
+    try {
+      dispatch({
+        type: LOAD_USERS_PAGING_REQUEST,
+      });
+
+      const res = await userService.getUsersPaging(currentPage);
+
+      dispatch({
+        type: LOAD_USERS_PAGING_SUCCESS,
+        payload: res,
+      });
+    } catch (error) {
+      dispatch({
+        type: LOAD_USERS_PAGING_FAILURE,
+        payload: { error: error.toString() },
+      });
+    }
+  };
+};
